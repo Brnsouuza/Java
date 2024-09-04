34506754abc194cb0f245ce5eebc815a21bb5e28
Brnsouuza <bruno-souza171@hotmail.com>
2024-09-04 16:27:02 -0300

    negadoida

---
+++ Aula-01/project.java
@@ -0,0 +1,22 @@
+public class project {
+
+    // Esse é o ponto de entrada do java
+    public static void main(String[] args) throws Exception{
+        //Todo final em comando java terminal em (;);
+        System.out.println("Hello,world!");
+        
+    }
+    
+}
+
+// No java nomes de CLASS seguem com letra maiúscula, seguindo a regra de camelcase.
+
+// CLASS = Declaração da classe, pois o java é orientado a um objeto.
+// Projet = Nome da minha class.
+// O restante são chamados de metódos ou funções que a class pode exercer. Ele é um ponto de entarda oara o programa.
+// Public statics = Modificador de Acesso do Método.
+// VOID, é o tipo de retorno do método.
+// MAIN, é o nome do método.
+// String ou args =  Parâmentors/Argumentos do método
+// System.out = Entrada do código
+
--- project.java
+++
@@ -1,7 +0,0 @@
-public class project {
-    public static void main(String[] args) throws Exception{
-        System.out.println("Hello,world!");
-        
-    }
-    
-}
