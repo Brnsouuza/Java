6e8e853e26feb74b25b0dc733fabc8ebfdafa20c
Brnsouuza <bruno-souza171@hotmail.com>
2025-05-20 11:53:23 -0300

    Aula faltante

---
+++ Argumentos.java
@@ -0,0 +1,6 @@
+
+class Argumentos {
+    public static void main(String[] args) {
+        System.out.println("Você digitou " + args[0]); /*Quando passado o args[0] ele esperar um argumento qualquer no momento da execução do código */
+    }
+}
